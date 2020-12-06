ea6e7ddd4d4ea1ab2f997991cb6ee0a97fb66abe
Federico <federico.taschin@studenti.unipd.it>
2020-12-06 20:52:52 +0100

    Fill findPartner() and create ohelper funcs

--- src/main/java/se/kth/jabeja/Jabeja.java
+++ src/main/java/se/kth/jabeja/Jabeja.java
@@ -42,6 +42,7 @@ public class Jabeja {
       //reduce the temperature
       saCoolDown();
       report();
+      System.out.println("Completed round " + round);
     }
   }
 
@@ -68,18 +69,18 @@ public class Jabeja {
 
     if (config.getNodeSelectionPolicy() == NodeSelectionPolicy.HYBRID
             || config.getNodeSelectionPolicy() == NodeSelectionPolicy.LOCAL) {
-        Node candidatePartner = findPartner(nodeId, (Integer[]) nodep.getNeighbours().toArray());
-        if (candidatePartner != null) {
-          partner = candidatePartner;
-        } else {
+        Integer[] neighbors = new Integer[nodep.getNeighbours().size()];
+        nodep.getNeighbours().toArray(neighbors);
+        Node candidatePartner = findPartner(nodeId, neighbors);
+        if (candidatePartner == null) {
           doRandom = true;
+        } else {
+          partner = candidatePartner;
         }
     }
 
-    if (doRandom || config.getNodeSelectionPolicy() == NodeSelectionPolicy.RANDOM) {
-      // if local policy fails then randomly sample the entire graph
+    if (doRandom || config.getNodeSelectionPolicy() == NodeSelectionPolicy.RANDOM)
       partner = findPartner(nodeId, getSample(nodeId));
-    }
 
     // swap the colors
     if (partner != null) {
@@ -90,15 +91,44 @@ public class Jabeja {
   }
 
   public Node findPartner(int nodeId, Integer[] nodes){
+    double alpha = config.getAlpha();
 
     Node nodep = entireGraph.get(nodeId);
-
     Node bestPartner = null;
     double highestBenefit = 0;
 
-    // TODO
-
+    // Iterate all nodes and get the one that maximizes
+    // utility if swapped with nodep
+    for (int idq: nodes) {
+      Node nodeq = entireGraph.get(idq);
+      double oldB = benefit(nodep, nodeq, alpha);
+      double newB = benefitSwap(nodep, nodeq, alpha);
+      if (newB * T > oldB || newB > highestBenefit) {
+        bestPartner = nodeq;
+        highestBenefit = newB;
+      }
+    }
     return bestPartner;
+  }
+
+  /**
+   * Compute the current benefit of two nodes. Benefit is defined as
+   * U(p, q) = d_p(p)^alpha + d_q(q)^alpha (Eq. 5 of the paper)
+   * where d_p(p) is the number of neighbors of p with its same color.
+   */
+  private double benefit(Node nodep, Node nodeq, double alpha) {
+    int dpp = getDegree(nodep, nodep.getColor());
+    int dqq = getDegree(nodeq, nodeq.getColor());
+    return Math.pow(dpp, alpha) + Math.pow(dqq, alpha);
+  }
+
+  /**
+   * Compute the benefit of the two nodes if their color was swapped.
+   */
+  private double benefitSwap(Node nodep, Node nodeq, double alpha) {
+    int dpq = getDegree(nodep, nodeq.getColor());
+    int dqp = getDegree(nodeq, nodep.getColor());
+    return Math.pow(dpq, alpha) + Math.pow(dqp, alpha);
   }
 
   /**
@@ -212,11 +242,12 @@ public class Jabeja {
 
     int edgeCut = grayLinks / 2;
 
+    /*
     logger.info("round: " + round +
             ", edge cut:" + edgeCut +
             ", swaps: " + numberOfSwaps +
             ", migrations: " + migrations);
-
+     */
     saveToFile(edgeCut, migrations);
   }
 
