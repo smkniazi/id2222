c1139e72da37408fa1ef3243b9b70f2fda106e63
Federico <federico.taschin@studenti.unipd.it>
2020-12-10 11:14:31 +0100

    Update run_tests.sh for new arguments

--- run_tests.sh
+++ run_tests.sh
@@ -1,28 +1,42 @@
 ./compile.sh
 
+# STANDARD JABEJA EXPERIMENTS
+# Temp = 1
+./run.sh -graph=graphs/3elt.graph -temp=1.0
+./run.sh -graph=graphs/add20.graph -temp=1.0
+./run.sh -graph=graphs/twitter.graph -temp=1.0
+./run.sh -graph=graphs/facebook.graph -temp=1.0
+
+# Temp = 2
+./run.sh -graph=graphs/3elt.graph -temp=1.0
+./run.sh -graph=graphs/add20.graph -temp=1.0
+./run.sh -graph=graphs/twitter.graph -temp=1.0
+./run.sh -graph=graphs/facebook.graph -temp=1.0
+
+# SIMULATED ANNEALING EXPERIMENTS
 # Experiments for different delta
-./run.sh -graph=graphs/3elt.graph -temp=1.0 -delta=0.9 -factor=1.0
-./run.sh -graph=graphs/add20.graph -temp=1.0 -delta=0.9 -factor=1.0
-./run.sh -graph=graphs/twitter.graph -temp=1.0 -delta=0.9 -factor=1.0
-./run.sh -graph=graphs/facebook.graph -temp=1.0 -delta=0.9 -factor=1.0
+./run.sh -graph=graphs/3elt.graph -temp=1.0 -sa -delta=0.9 -factor=1.0
+./run.sh -graph=graphs/add20.graph -temp=1.0 -sa -delta=0.9 -factor=1.0
+./run.sh -graph=graphs/twitter.graph -temp=1.0 -sa -delta=0.9 -factor=1.0
+./run.sh -graph=graphs/facebook.graph -temp=1.0 -sa -delta=0.9 -factor=1.0
 
-./run.sh -graph=graphs/3elt.graph -temp=1.0 -delta=0.95 -factor=1.0
-./run.sh -graph=graphs/add20.graph -temp=1.0 -delta=0.95 -factor=1.0
-./run.sh -graph=graphs/twitter.graph -temp=1.0 -delta=0.95 -factor=1.0
+./run.sh -graph=graphs/3elt.graph -temp=1.0 -sa -delta=0.95 -factor=1.0
+./run.sh -graph=graphs/add20.graph -temp=1.0 -sa -delta=0.95 -factor=1.0
+./run.sh -graph=graphs/twitter.graph -temp=1.0 -sa -delta=0.95 -factor=1.0
 
-./run.sh -graph=graphs/3elt.graph -temp=1.0 -delta=0.97 -factor=1.0
-./run.sh -graph=graphs/add20.graph -temp=1.0 -delta=0.97 -factor=1.0
-./run.sh -graph=graphs/twitter.graph -temp=1.0 -delta=0.97 -factor=1.0
+./run.sh -graph=graphs/3elt.graph -temp=1.0 -sa -delta=0.97 -factor=1.0
+./run.sh -graph=graphs/add20.graph -temp=1.0 -sa -delta=0.97 -factor=1.0
+./run.sh -graph=graphs/twitter.graph -temp=1.0 -sa -delta=0.97 -factor=1.0
 
-./run.sh -graph=graphs/3elt.graph -temp=1.0 -delta=0.99 -factor=1.0
-./run.sh -graph=graphs/add20.graph -temp=1.0 -delta=0.99 -factor=1.0
-./run.sh -graph=graphs/twitter.graph -temp=1.0 -delta=0.99 -factor=1.0
+./run.sh -graph=graphs/3elt.graph -temp=1.0 -sa -delta=0.99 -factor=1.0
+./run.sh -graph=graphs/add20.graph -temp=1.0 -sa -delta=0.99 -factor=1.0
+./run.sh -graph=graphs/twitter.graph -temp=1.0 -sa -delta=0.99 -factor=1.0
 
 # Experiments with different factors
-./run.sh -graph=graphs/3elt.graph -temp=1.0 -delta=0.9 -factor=0.5
-./run.sh -graph=graphs/add20.graph -temp=1.0 -delta=0.9 -factor=0.5
-./run.sh -graph=graphs/twitter.graph -temp=1.0 -delta=0.9 -factor=0.5
+./run.sh -graph=graphs/3elt.graph -temp=1.0 -sa -delta=0.9 -factor=0.5
+./run.sh -graph=graphs/add20.graph -temp=1.0 -sa -delta=0.9 -factor=0.5
+./run.sh -graph=graphs/twitter.graph -temp=1.0 -sa -delta=0.9 -factor=0.5
 
-./run.sh -graph=graphs/3elt.graph -temp=1.0 -delta=0.9 -factor=1.5
-./run.sh -graph=graphs/add20.graph -temp=1.0 -delta=0.9 -factor=1.5
-./run.sh -graph=graphs/twitter.graph -temp=1.0 -delta=0.9 -factor=1.5
+./run.sh -graph=graphs/3elt.graph -temp=1.0 -sa -delta=0.9 -factor=1.5
+./run.sh -graph=graphs/add20.graph -temp=1.0 -sa -delta=0.9 -factor=1.5
+./run.sh -graph=graphs/twitter.graph -temp=1.0 -sa -delta=0.9 -factor=1.5
